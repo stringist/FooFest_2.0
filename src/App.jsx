3dedc6e0e59413a3fd3e95a59d00804eb7ff494d
Lasse Weber Joergensen <lass876q@stud.kea.dk>
2022-05-10 16:02:55 +0200

    1-commit, react_reroute,  + basic folder structure

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import Festival from "./roots/Festival";
+import Booking from "./roots/Booking";
+import "./App.css";
+
+import { Link, Routes, Route } from "react-router-dom";
+// These are for the react-reroute
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <header>
+        <nav>
+          <Link to="/">Festival</Link> | <Link to="/Booking">Booking</Link>
+        </nav>
+      </header>
+      <Routes>
+        <Route path="/" element={<Festival />} />
+        <Route path="/Booking" element={<Booking />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
