e5fad3f0e9be817ea2970bb9d5cfadc1fa3033fa
paulafiaschi <paul6399@stud.kea.dk>
2022-05-12 17:56:00 +0200

    useEffect added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,47 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+// import filter from "just-filter";
+
 import Festival from "./roots/Festival";
 import Booking from "./roots/Booking";
 import "./App.css";
-
 import { Link, Routes, Route } from "react-router-dom";
 // These are for the react-reroute
+
+import Lineup from "./components/Lineup";
+
 function App() {
   const [count, setCount] = useState(0);
+  const [schedule, setSchedule] = useState([]);
+
+  useEffect(() => {
+    fetch(`https://foofestival.herokuapp.com/schedule`)
+      .then((res) => res.json())
+      .then((data) => {
+        // console.log(data);
+        setSchedule(data);
+      });
+  }, []);
+
+  // const [events, setEvents] = useState([]);
+
+  // useEffect(() => {
+  //   fetch(`https://foofestival.herokuapp.com/events`)
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       console.log(data);
+  //       setEvents(data);
+  //     });
+  // }, []);
+  // console.log(events);
+
+  // console.log(schedule);
+  // console.log(schedule.Midgard);
+  // Object.values(schedule).map((x) => console.log(x));
+  // Object.keys(schedule).map((x) => console.log(x));
+  // Object.entries(schedule).map((x) => console.log(x));
+  const midgard = schedule.Midgard;
+  const vanaheim = schedule.Vanaheim;
+  const jotunheim = schedule.Jotunheim;
 
   return (
     <div className="App">
@@ -19,6 +54,23 @@ function App() {
         <Route path="/" element={<Festival />} />
         <Route path="/Booking" element={<Booking />} />
       </Routes>
+
+      <section className="stages">
+        <div className="days">
+          <ul>
+            <li>Mon</li>
+            <li>Tue</li>
+            <li>Wed</li>
+            <li>Thu</li>
+            <li>Fri</li>
+            <li>Sat</li>
+            <li>Sun</li>
+          </ul>
+        </div>
+        <Lineup {...midgard}></Lineup>
+        <Lineup {...jotunheim}></Lineup>
+        <Lineup {...vanaheim}></Lineup>
+      </section>
     </div>
   );
 }
