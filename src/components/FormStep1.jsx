f4bde2286c4af70c97a5cc1b655cc410ec8c5741
paulafiaschi <paul6399@stud.kea.dk>
2022-05-20 15:39:56 +0200

    alert pop up working

--- src/components/FormStep1.jsx
+++ src/components/FormStep1.jsx
@@ -2,6 +2,7 @@ import { useRef, useState } from "react";
 import AreaButton from "./AreaButton";
 import TicketButton from "./TicketButton";
 import AmountButton from "./AmountButton";
+import Alert from "./Alert";
 
 export default function FormStep1() {
   const formEl = useRef(null);
@@ -11,6 +12,7 @@ export default function FormStep1() {
   const [amount, setAmount] = useState(1);
   const [isSearching, setIsSearching] = useState(false);
   const [reservation, setReservation] = useState([]);
+  const [showAlert, setAlert] = useState(false);
 
   function searchTickets(e) {
     e.preventDefault();
@@ -31,51 +33,52 @@ export default function FormStep1() {
     })
       .then((response) => {
         response.json().then((data) => {
-          // console.log(data);
           setReservation(data);
+          data.error ? setAlert(true) : setAlert(false);
         });
+
         setIsSearching(false);
+        console.log(showAlert);
       })
       .catch((err) => console.error(err));
   }
   console.log(reservation);
 
-  if (reservation.error === "Invalid area, expired id or not enough available spots") {
-    return alert("no available spots, try another area");
-  } else if (reservation.message === "Reserved") {
-    return alert("tickets resered");
-  }
-
   return (
-    <form ref={formEl} onSubmit={searchTickets}>
-      <fieldset>
-        <legend>Choose ticket</legend>
-        <TicketButton name="General" price="799kr" ticket={ticket} setTicket={setTicket} />
-        <TicketButton name="VIP" price="1299kr" ticket={ticket} setTicket={setTicket} />
-      </fieldset>
+    <>
+      <form ref={formEl} onSubmit={searchTickets}>
+        <fieldset>
+          <legend>Choose ticket</legend>
+          <TicketButton name="General" price="799kr" ticket={ticket} setTicket={setTicket} />
+          <TicketButton name="VIP" price="1299kr" ticket={ticket} setTicket={setTicket} />
+        </fieldset>
 
-      <fieldset>
-        <legend>Choose area</legend>
-        <div className="form__area">
-          <div className="form__area--row">
-            <AreaButton name="Svartheim" area={area} setArea={setArea} />
-            <AreaButton name="Nilfheim" area={area} setArea={setArea} />
-            <AreaButton name="Helheim" area={area} setArea={setArea} />
+        <fieldset>
+          <legend>Choose area</legend>
+          <div className="form__area">
+            <div className="form__area--row">
+              <AreaButton name="Svartheim" area={area} setArea={setArea} />
+              <AreaButton name="Nilfheim" area={area} setArea={setArea} />
+              <AreaButton name="Helheim" area={area} setArea={setArea} />
+            </div>
+            <div className="form__area--row">
+              <AreaButton name="Muspeleheim" area={area} setArea={setArea} />
+              <AreaButton name="Alfheim" area={area} setArea={setArea} />
+            </div>
           </div>
-          <div className="form__area--row">
-            <AreaButton name="Muspeleheim" area={area} setArea={setArea} />
-            <AreaButton name="Alfheim" area={area} setArea={setArea} />
-          </div>
-        </div>
-      </fieldset>
+        </fieldset>
 
-      <fieldset>
-        <legend>Choose amount</legend>
-        <AmountButton name="amount" amount={amount} setAmount={setAmount} />
-      </fieldset>
+        <fieldset>
+          <legend>Choose amount</legend>
+          <AmountButton name="amount" amount={amount} setAmount={setAmount} />
+        </fieldset>
 
-      {!isSearching && <button>Search tickets</button>}
-      {isSearching && <button>Searching tickets...</button>}
-    </form>
+        {!isSearching && <button>Search tickets</button>}
+        {isSearching && <button>Searching tickets...</button>}
+      </form>
+
+      {showAlert && <Alert message={reservation.error} setAlert={setAlert} />}
+      {/* {showAlert === true ? <Alert message={reservation.error} setAlert={setAlert} /> : <FormStep2/>} */}
+    </>
   );
 }
