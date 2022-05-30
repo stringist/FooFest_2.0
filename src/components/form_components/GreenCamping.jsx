7914ce9d907a3077944108a9f264754f6ed4c5bb
Kalle Tiihonen <kalletiihonen@hotmail.com>
2022-05-30 10:20:09 +0200

    checkbox works

--- src/components/form_components/GreenCamping.jsx
+++ src/components/form_components/GreenCamping.jsx
@@ -1,39 +1,18 @@
-import { useState } from "react";
-
 export default function GreenCamping({ basket, setBasket }) {
-  const [isGreen, setIsGreen] = useState(false);
-  const [isChecked, setIsChecked] = useState(false);
+  const updateBasket = () => {
+    const product = { product: "Green Camping", price: 249, id: 666 };
 
-  function updateBasket() {
-    // console.log(`Checked is ${isGreen}, isGreen is ${isGreen}, basket is ${basket[0]}`);
-console.log( isChecked, isGreen, basket);
-{isGreen? addToBasket() : removeFromBasket();}
-  }
-
-  const addToBasket = () => {
-    console.log("adding to basket");
-    //   setBasket((prevState) => prevState.concat({ product: "Green Camping", price: 249, id: 666}));
-      setBasket((prevState) => [...prevState, { product: "Green Camping", price: 249, id: 666 }])
-  };
-
-  const removeFromBasket = () => {
-    console.log("removing from basket");
-    // setBasket((old) => old.filter((item) => item.id !== id));
-  };
-
-  const handleChange = (e) => {console.log(e.target.checked);
-    // setIsChecked((prevState) => prevState = !prevState);
-    // setIsChecked(!isChecked);
-    // setIsGreen(!isChecked);
-
-setIsChecked(prevState => !prevState);
-    setIsGreen(prevState => prevState = !isChecked);
-    updateBasket();
+// checks to see if item is already in basket; removes if it is
+    {
+      basket.find((item) => item.id === product.id)
+        ? setBasket((prevState) => prevState.filter((item) => item.id !== product.id))
+        : setBasket((old) => [...old, product]);
+    }
   };
 
   return (
     <div className="formItem">
-      <input type="checkbox" id="greenCamping" name="greenCamping"  checked={isChecked} onChange={handleChange}/>
+      <input type="checkbox" id="greenCamping" name="greenCamping" onChange={updateBasket} />
       <div className="text_content">
         <label htmlFor="greenCamping">Green camping </label>
         <p className="subtext">
