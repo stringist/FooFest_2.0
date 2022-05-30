202f1c937e79f443b3e12ac3e5d5444be1626807
Kalle Tiihonen <kalletiihonen@hotmail.com>
2022-05-30 21:48:57 +0200

    basket list works minus ticket price

--- src/components/form_components/BasketList.jsx
+++ src/components/form_components/BasketList.jsx
@@ -6,7 +6,7 @@ export default function BasketList(props) {
     <li>
       {props.amount}x {props.ticket}, {props.area} <span className="price">{799 * props.amount} Dkk</span>
     </li>;
-  const basketItems = props.basket.map((item)=> <li>{item.amount}x {item.product} <span className="price">{item.price * item.amount} Dkk</span></li>);
+  const basketItems = props.basket.map((item)=> item.amount > 0 ? <li>{item.amount}x {item.product} <span className="price">{item.price * item.amount} Dkk</span></li> : null);
   return (
     <>
       <ul>
--- src/components/form_components/Total.jsx
+++ src/components/form_components/Total.jsx
@@ -1,11 +1,12 @@
 export default function Total(props) {
   const initialValue = 0;
-  const sumWithInital = props.basket.reduce((prevValue, curValue) => prevValue + curValue.amount * curValue.price, initialValue);
+  const sumWithInitial = props.basket.reduce((prevValue, curValue) => prevValue + curValue.amount * curValue.price, initialValue);
+// const totalWithTicket = sumWithInital + (props.amount * props.price);
   console.log(props);
   return (
     <div className="total">
       <p>Total:</p>
-      <p>{sumWithInital}Dkk</p>
+      <p>{sumWithInitial}Dkk</p>
     </div>
   );
 }
