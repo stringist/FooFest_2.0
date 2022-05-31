b215c31a2eb4917737b77c5ab234179ae314d68b
Kalle Tiihonen <kalletiihonen@hotmail.com>
2022-05-31 22:19:48 +0200

    form items styling-need checkbox

--- src/components/form_components/Summary.jsx
+++ src/components/form_components/Summary.jsx
@@ -1,8 +1,9 @@
 import BasketList from "./BasketList";
 import Total from "./Total";
+import SummaryStyles from "/sass/modules/_Summary.module.scss"
 
 export default function Summary(props) {
-  return (<section>
+  return (<section className={SummaryStyles.summary}>
 <h3>Summary</h3>
 <BasketList {...props}/>
 <Total {...props}/>
