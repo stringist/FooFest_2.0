bbe75fc5c7a939ebbdb8d06eea7ed6064e3844c1
Kalle Tiihonen <kalletiihonen@hotmail.com>
2022-05-31 08:20:50 +0200

    fixing basket

--- src/components/form_components/Total.jsx
+++ src/components/form_components/Total.jsx
@@ -1,6 +1,7 @@
 export default function Total(props) {
   const initialValue = 0;
   const sumWithInitial = props.basket.reduce((prevValue, curValue) => prevValue + curValue.amount * curValue.price, initialValue);
+
 // const totalWithTicket = sumWithInital + (props.amount * props.price);
   console.log(props);
   return (
