630d3be385eea19c0514a617c52d22919011909f
Lasse Weber Joergensen <lass876q@stud.kea.dk>
2022-05-31 12:07:15 +0200

    creditcard npm installed again should wokr

--- src/components/CCvalidation.jsx
+++ src/components/CCvalidation.jsx
@@ -14,22 +14,21 @@ export default function CCvalidation(props) {
   const formEl = useRef(null);
   const ENDPOINT = "https://kea2semester-e216.restdb.io/rest/foofest";
   const KEY = "615d83068597142da1745455";
-
   function changeFocus(e) {
     const attr = parseInt(e.target.attributes.maxLength.value, 10);
     // if attrb === undefined - because the input for Name doesnt carry a maxlength
     if (attr === undefined) {
     } else if (e.target.textLength === attr) {
-      e.target.nextElementSibling.focus();
-      if (e.target.nextElementSibling.nodeName === "SECTION") {
-        e.target.nextElementSibling.firstChild.focus();
-        // the if statement underneath here is because expiry + cvc was put into a section
+      if (e.target.nextElementSibling === null) {
+        e.target.parentElement.nextElementSibling.nextElementSibling.focus();
+        console.log(
+          e.target.parentElement.nextElementSibling.nextElementSibling
+        );
+      } else {
+        e.target.nextElementSibling.nextElementSibling.focus();
       }
-    } else if (e.target.nextElementSibling === null) {
-      console.log("fack");
     }
   }
-
   function handleCardDisplay() {
     const rawText = [...number.split(" ").join("")]; // Remove old space
     const creditCard = []; // Create card as array
@@ -39,48 +38,47 @@ export default function CCvalidation(props) {
     });
     return creditCard.join(""); // Transform card array to string
   }
-
   function handleExpiryDate(e) {
-    // if (formEl.current !== null) {
-    //   console.log(e);
-    //   formEl.current.elements.expiry.value = formEl.current.elements.expiry.value
-    //     .replace(
-    //       /^([1-9]\/|[2-9])$/g,
-    //       "0$1/" // 3 > 03/
-    //     )
-    //     .replace(
-    //       /^(0[1-9]|1[0-2])$/g,
-    //       "$1/" // 11 > 11/
-    //     )
-    //     .replace(
-    //       /^([0-1])([3-9])$/g,
-    //       "0$1/$2" // 13 > 01/3
-    //     )
-    //     .replace(
-    //       /^(0?[1-9]|1[0-2])([0-9]{2})$/g,
-    //       "$1/$2" // 141 > 01/41
-    //     )
-    //     .replace(
-    //       /^([0]+)\/|[0]+$/g,
-    //       "0" // 0/ > 0 and 00 > 0
-    //     )
-    //     .replace(
-    //       /[^\d\/]|^[\/]*$/g,
-    //       "" // To allow only digits and `/`
-    //     )
-    //     .replace(
-    //       /\/\//g,
-    //       "/" // Prevent entering more than 1 `/`
-    //     );
-    // }
+    /*     console.log(formEl.current.elements.expiry.value);
+     */ if (formEl.current === null) {
+    } else {
+      formEl.current.elements.expiry.value =
+        formEl.current.elements.expiry.value
+          .replace(
+            /^([1-9]\/|[2-9])$/g,
+            "0$1/" // 3 > 03/
+          )
+          .replace(
+            /^(0[1-9]|1[0-2])$/g,
+            "$1/" // 11 > 11/
+          )
+          .replace(
+            /^([0-1])([3-9])$/g,
+            "0$1/$2" // 13 > 01/3
+          )
+          .replace(
+            /^(0?[1-9]|1[0-2])([0-9]{2})$/g,
+            "$1/$2" // 141 > 01/41
+          )
+          .replace(
+            /^([0]+)\/|[0]+$/g,
+            "0" // 0/ > 0 and 00 > 0
+          )
+          .replace(
+            /[^\d\/]|^[\/]*$/g,
+            "" // To allow only digits and `/`
+          )
+          .replace(
+            /\/\//g,
+            "/" // Prevent entering more than 1 `/`
+          );
+    }
   }
-
   function changePage(e) {
     e.preventDefault();
     console.log(props.ticketholderdata);
     props.setShowContent(0);
   }
-
   function handleSubmit(e) {
     e.preventDefault();
 
@@ -101,17 +99,30 @@ export default function CCvalidation(props) {
     <>
       <img src={step4} alt="" />
       <h4 className={Creditcard.cc_h4}>CARD INFORMATION</h4>
-      <Cards number={number} name={name} expiry={expiry} cvc={cvc} focused={focus}></Cards>
+      {
+        <Cards
+          number={number}
+          name={name}
+          expiry={expiry}
+          cvc={cvc}
+          focused={focus}
+        ></Cards>
+      }
 
-      <form ref={formEl} onChange={changeFocus} onSubmit={handleSubmit} className={Creditcard.cc_form}>
+      <form
+        ref={formEl}
+        onChange={changeFocus}
+        onSubmit={handleSubmit}
+        className={Creditcard.cc_form}
+      >
         <article>
           {" "}
           <span>Name</span>
           <input
             type="text"
             name="name"
-            /*           value={name}
-             */ onChange={(e) => setName(e.target.value)}
+            value={name}
+            onChange={(e) => setName(e.target.value)}
             onFocus={(e) => setFocus(e.target.name)}
             minLength="1"
             maxLength="45"
@@ -121,22 +132,61 @@ export default function CCvalidation(props) {
         <article>
           {" "}
           <span>Card Number</span>
-          <input type="tel" name="number" value={handleCardDisplay()} onChange={(e) => setNumber(e.target.value)} onFocus={(e) => setFocus(e.target.name)} onInput={(e) => handleCardDisplay()} minLength="16" maxLength="19" required />
+          <input
+            type="tel"
+            name="number"
+            value={handleCardDisplay()}
+            onChange={(e) => setNumber(e.target.value)}
+            onFocus={(e) => setFocus(e.target.name)}
+            onInput={(e) => handleCardDisplay()}
+            minLength="16"
+            maxLength="19"
+            required
+          />
         </article>
 
-        <section className={Creditcard.section_cc}>
-          <span>Expiry</span>
-          <input type="text" value={handleExpiryDate()} onChange={(e) => setExpiry(e.target.value)} onFocus={(e) => setFocus(e.target.name)} onInput={(e) => handleExpiryDate(e)} minLength="5" maxLength="5" required />
-          <span>CVC</span>
-          {number.substring(0, 2) == 34 || number.substring(0, 2) == 37 ? (
-            <input type="tel" name="cvc" value={cvc} onChange={(e) => setCvc(e.target.value)} onFocus={(e) => setFocus(e.target.name)} minLength="3" maxLength="4" required />
-          ) : (
-            <input type="tel" name="cvc" value={cvc} onChange={(e) => setCvc(e.target.value)} onFocus={(e) => setFocus(e.target.name)} minLength="3" maxLength="3" required />
-          )}
-        </section>
+        <span>Expiry</span>
+        <input
+          name="expiry"
+          type="text"
+          value={handleExpiryDate()}
+          onChange={(e) => setExpiry(e.target.value)}
+          onFocus={(e) => setFocus(e.target.name)}
+          onInput={(e) => handleExpiryDate(e)}
+          minLength="5"
+          maxLength="5"
+          required
+        />
+        <span>CVC</span>
+        {number.substring(0, 2) == 34 || number.substring(0, 2) == 37 ? (
+          <input
+            type="tel"
+            name="cvc"
+            value={cvc}
+            onChange={(e) => setCvc(e.target.value)}
+            onFocus={(e) => setFocus(e.target.name)}
+            minLength="3"
+            maxLength="4"
+            required
+          />
+        ) : (
+          <input
+            type="tel"
+            name="cvc"
+            value={cvc}
+            onChange={(e) => setCvc(e.target.value)}
+            onFocus={(e) => setFocus(e.target.name)}
+            minLength="3"
+            maxLength="3"
+            required
+          />
+        )}
         <section className={generalStyles.next_back_buttons}>
           <button onClick={changePage}>Back</button>
-          <button onSubmit={(e) => handleSubmit()} onFocus={(e) => setFocus(e.target.name)}>
+          <button
+            onSubmit={(e) => handleSubmit()}
+            onFocus={(e) => setFocus(e.target.name)}
+          >
             Complete purchase
           </button>
         </section>
