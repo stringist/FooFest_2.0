e091cb2aa6d8e9b55c5aec12433886ca63ec64f3
Kalle Tiihonen <kalletiihonen@hotmail.com>
2022-05-31 10:33:33 +0200

    total counter works

--- src/components/form_components/Total.jsx
+++ src/components/form_components/Total.jsx
@@ -1,13 +1,20 @@
 export default function Total(props) {
-  const initialValue = 0;
-  const sumWithInitial = props.basket.reduce((prevValue, curValue) => prevValue + curValue.amount * curValue.price, initialValue);
+  let ticketPrice;
 
-// const totalWithTicket = sumWithInital + (props.amount * props.price);
+  {
+    props.ticket === "General" ? (ticketPrice = props.amount * 799) : (ticketPrice = props.amount * 1299);
+  }
+  const initialValue = 0;
+  const sumWithInitial = props.basket.reduce(
+    (prevValue, curValue) => prevValue + curValue.amount * curValue.price,
+    initialValue
+  );
+  const totalPrice = ticketPrice + sumWithInitial;
   console.log(props);
   return (
     <div className="total">
       <p>Total:</p>
-      <p>{sumWithInitial}Dkk</p>
+      <p>{totalPrice}Dkk</p>
     </div>
   );
 }
