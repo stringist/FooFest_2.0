9e055313b8a8fb146900132d2b6643c36f07a57a
Kalle Tiihonen <kalletiihonen@hotmail.com>
2022-06-02 21:52:00 +0200

    fix BandPage

--- src/components/artists/BandPage.jsx
+++ src/components/artists/BandPage.jsx
@@ -20,6 +20,13 @@ export default function BandPage(props) {
   if (band.name != undefined) {
     return (
       <div className={detail.bandDetail}>
+
+
+
+
+ 
+
+
         <button onClick={() => props.setBandDisplayed([null])}>
           <svg xmlns="http://www.w3.org/2000/svg" height="60" width="60">
             <path d="M12.45 37.65 10.35 35.55 21.9 24 10.35 12.45 12.45 10.35 24 21.9 35.55 10.35 37.65 12.45 26.1 24 37.65 35.55 35.55 37.65 24 26.1Z" />
