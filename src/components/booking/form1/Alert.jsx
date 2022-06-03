f8eb78c53dc0ebece1ca05cb8ee7bf49d694a51e
Kalle Tiihonen <kalletiihonen@hotmail.com>
2022-06-03 09:10:54 +0200

    alert tweaked

--- src/components/booking/form1/Alert.jsx
+++ src/components/booking/form1/Alert.jsx
@@ -10,7 +10,7 @@ export default function Alert(props) {
           <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
         </svg>
       </button>
-      <p>Oh, no! It looks like there is not enough place on the area selected.</p>
+      <p>Oh, no! It looks like there aren't any spaces available in the area selected.</p>
       <p>Please, choose another area</p>
     </div>
   );
