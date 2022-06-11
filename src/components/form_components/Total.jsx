7fd455389180501e63e95b3136344acca5c7b78e
Kalle Tiihonen <kalletiihonen@hotmail.com>
2022-06-11 14:07:21 +0200

    'remove' button not on booking fee

--- src/components/form_components/Total.jsx
+++ src/components/form_components/Total.jsx
@@ -1,19 +1,19 @@
 import summaryStyles from "/sass/modules/_Summary.module.scss";
 
 export default function Total(props) {
-  let ticketPrice;
+  // let ticketPrice;
 
-  {
-    props.ticket === "General"
-      ? (ticketPrice = props.amount * 799)
-      : (ticketPrice = props.amount * 1299);
-  }
+  // {
+  //   props.ticket === "General"
+  //     ? (ticketPrice = props.amount * 799)
+  //     : (ticketPrice = props.amount * 1299);
+  // }
   const initialValue = 0;
   const sumWithInitial = props.basket.reduce(
     (prevValue, curValue) => prevValue + curValue.amount * curValue.price,
     initialValue
   );
-  const totalPrice = ticketPrice + sumWithInitial;
+  const totalPrice = sumWithInitial;
   return (
     <div className={summaryStyles.total}>
       <p>Total:</p>
